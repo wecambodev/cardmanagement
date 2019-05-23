50fbd216cc5b1e3843fdd3f2c39864ce1c94e01d
phuongphally <phuongphally@gmail.com>
2019-05-23 21:30:27 +0700

    First commit

--- src/main/java/com/mbanq/cardmanagement/model/Role.java
+++ src/main/java/com/mbanq/cardmanagement/model/Role.java
@@ -1,22 +1,16 @@
 package com.mbanq.cardmanagement.model;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.NaturalId;
-
 import javax.persistence.*;
 
 @Entity
 @Table(name = "roles")
-@AllArgsConstructor
-@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    @NaturalId
+
     @Column(length = 60)
     private RoleName name;
 
